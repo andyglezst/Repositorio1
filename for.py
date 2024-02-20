67c9680656bde4edf05fe76fe7acfae70d76a806
andyglezst <159494193+andyglezst@users.noreply.github.com>
2024-02-20 17:27:42 -0600

    Clase 2002.2

---
+++ for.py
@@ -0,0 +1,12 @@
+numero1 =  int(input("Ingresa el numero 1:"))
+numero2 =  int(input("Ingresa el numero 2:"))
+table = range(numero1, numero2)
+for numero in table:
+    if numero %2 == 0 :
+        print(f"Este numero es par {numero}")
+    else: 
+        print(f"Este numero es impar {numero}")
+
+nombre = "Andy"
+for letra in nombre:
+    print(letra)
